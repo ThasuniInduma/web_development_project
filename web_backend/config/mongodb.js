43551c7a8b1256242b04977c5e6e380844f07b2b
Thasuni Induma <138390612+ThasuniInduma@users.noreply.github.com>
2025-07-29 00:05:55 +0530

    backend logic done

--- web_backend/config/mongodb.js
+++ web_backend/config/mongodb.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
 
     mongoose.connection.on('connected', () => console.log("Database Connected"));
 
-    await mongoose.connect(`${process.env.MONGODB_URI}/mern-auth`);
+    await mongoose.connect(`${process.env.MONGODB_URI}/web-project`);
 
 }
 export default connectDB;
