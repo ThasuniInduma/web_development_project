f41f9a8e01499e3d7ab4eefaab9d1abb55a95a50
Thasuni Induma <138390612+ThasuniInduma@users.noreply.github.com>
2025-07-29 17:21:28 +0530

    create home page

--- web_frontend/src/App.jsx
+++ web_frontend/src/App.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
